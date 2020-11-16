adc2ee2e83b3cc7376e6292448d1ad11d76fbd9b
DABax47 <45886982+DABax47@users.noreply.github.com>
2020-11-16 14:01:29 -0500

    Updates

---
+++ precache-manifest.971be90e02468fad5fd67db60f8cdadf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4525d0de8775ea1d2d7449a260f935",
+    "url": "/stox/index.html"
+  },
+  {
+    "revision": "2fe66adfd74e843dda3f",
+    "url": "/stox/static/css/main.a71bec85.chunk.css"
+  },
+  {
+    "revision": "4817d9f09186f858f05c",
+    "url": "/stox/static/js/2.9ee41e3d.chunk.js"
+  },
+  {
+    "revision": "9f3e154b5a21336fa161f9ed4e97157c",
+    "url": "/stox/static/js/2.9ee41e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe66adfd74e843dda3f",
+    "url": "/stox/static/js/main.dfbd8c53.chunk.js"
+  },
+  {
+    "revision": "16a7381d21fae716f681",
+    "url": "/stox/static/js/runtime-main.42151878.js"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/stox/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "972d54ac7985dbc9a8014a49a29c7668",
+    "url": "/stox/static/media/ShareTechMono-Regular.972d54ac.ttf"
+  }
+]);
